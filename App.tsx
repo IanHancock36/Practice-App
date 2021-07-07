77374af2285593d1440039fbbbf5c60107e1ced7
Ian Hancock <northroadian@gmail.com>
2021-07-07 06:51:12 -0500

    added styling

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import Header from './Components/Header';
 
-export default function App() {
+const App =() => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style = {styles.container}>
+      <Header title='shopping List'/>
+    </SafeAreaView>
+  
+  
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
+    backgroundColor: '#e8e7e3'
   },
 });
+export default App
---
+++ Components/AddItem.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native'
+
+export interface Item {
+    item: string;
+    quantity: string;
+}
+
+export const AddItem = () => {
+    const [item, setItem] = useState('')
+    const [quantity, setQuantity] = useState('')
+
+    return (
+        <View>
+            <Text style={styles.heading}>Add Shopping Item </Text>
+            <View style={styles.form}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter Item"
+                    value={item}
+                    onChangeText={text => setItem(text)}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Enter Quantity'
+                    keyboardType='numeric'
+                    value='quantity'
+                    onChangeText={quantity => { setQuantity(quantity) }}
+
+                />
+                <TouchableOpacity style={styles.addItemButton} onPress={() => { }}>
+                    <Text style={styles.buttonText}>Add Item</Text>
+                    </TouchableOpacity>
+            </View>
+            </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    heading :{
+        fontSize: 20,
+        fontWeight: '700',
+    },
+    form: {
+        marginTop: 30,
+    },
+    input:{
+        padding: 15, 
+        borderColor: '#39FF14',
+        borderWidth: 1, 
+        borderRadius: 5,
+        marginBottom: 20,
+
+    },
+    addItemButton:{
+        backgroundColor:'#FF10F0',
+        paddingVertical: 20,
+        borderRadius: 5,
+        alignItems:'center'
+    },
+    buttonText:{
+        color: '#fff',
+        fontWeight: '500'
+
+    },
+})
